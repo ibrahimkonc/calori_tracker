a583d58fd4b2523a02c9d574417945bbac4265f7
ibrahimkonc <iboknc238@gmail.com>
2023-03-22 14:42:47 +0300

    default setting change

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'package:calori_tracker/providers/system_provider.dart';
 import 'package:calori_tracker/screens/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(MultiProvider(
+    providers: [
+      ListenableProvider(create: (_) => SystemProvider()),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
