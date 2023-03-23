0f2d9a268078651da1e15ca0332149c15d298b75
Gentle <muhammetdoger@hotmail.com>
2023-03-23 22:44:49 +0300

    design

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,27 +1,61 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  bool userGender = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profile Page"),
-        backgroundColor: Colors.grey,
-      ),
+      appBar: userGender == true
+          ? AppBar(
+              centerTitle: true,
+              title: Text("Profile Page"),
+              backgroundColor: Colors.blue,
+            )
+          : AppBar(
+              centerTitle: true,
+              title: Text("Profile Page"),
+              backgroundColor: Colors.pink,
+            ),
       body: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "assets/images/men.png",
-                width: 250,
-                height: 250,
-                fit: BoxFit.fill,
-              ),
-            ],
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  userGender = !userGender;
+                });
+              },
+              icon: Icon(Icons.change_circle_outlined)),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                userGender == true
+                    ? Image.asset(
+                        "assets/images/men.png",
+                        width: 250,
+                        height: 250,
+                        fit: BoxFit.fill,
+                      )
+                    : Image.asset(
+                        "assets/images/women.png",
+                        width: 250,
+                        height: 250,
+                        fit: BoxFit.fill,
+                      ),
+              ],
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 15.0),
