50e7a6afc8db389cd0220196d5c14d18c64f98a8
melberro <melberro57@gmail.com>
2023-03-23 11:31:32 +0300

    bottomnavbar

---
+++ lib/components/bottom_navbar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class BottomNavbar extends StatelessWidget {
+  final int pageIndex;
+  final Function(int value) onTap;
+  const BottomNavbar({super.key, required this.pageIndex, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Container(
+      child: Stack(
+        children: [
+          CustomPaint(
+            size: Size(size.width, 80),
+            painter: BNBCustomePainter(),
+          ),
+          Center(
+            heightFactor: 0.6,
+            child: FloatingActionButton(
+              onPressed: () => onTap(1),
+              backgroundColor: Colors.orange,
+              child: Icon(Icons.home),
+              elevation: 0.1,
+            ),
+          ),
+          SizedBox(
+            width: size.width,
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(onPressed: () => onTap(0), icon: Icon(Icons.add)),
+                Container(
+                  width: size.width * .20,
+                ),
+                IconButton(onPressed: () => onTap(2), icon: Icon(Icons.person)),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BNBCustomePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+    Path path = Path()..moveTo(0, 20);
+    path.quadraticBezierTo(size.width * .20, 0, size.width * .35, 0);
+    path.quadraticBezierTo(size.width * .40, 0, size.width * .40, 20);
+    path.arcToPoint(Offset(size.width * .60, 20),
+        radius: Radius.circular(10.0), clockwise: false);
+
+    path.quadraticBezierTo(size.width * .60, 0, size.width * .65, 0);
+    path.quadraticBezierTo(size.width * .80, 0, size.width, 20);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    canvas.drawShadow(path, Colors.black, 5, true);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:calori_tracker/providers/system_provider.dart';
 import 'package:calori_tracker/screens/home.dart';
 import 'package:calori_tracker/providers/theme_provider.dart';
 import 'package:calori_tracker/screens/login.dart';
+import 'package:calori_tracker/screens/test_home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -29,10 +30,10 @@ class MyApp extends StatelessWidget {
     final currentTheme = Provider.of<ThemeChanger>(context).currentTheme;
 
     return MaterialApp(
-      theme: currentTheme,
-      debugShowCheckedModeBanner: false,
+      //  theme: currentTheme,
+      // debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: const LoginPage(),
+      home: MyHomePage(),
     );
   }
 }
